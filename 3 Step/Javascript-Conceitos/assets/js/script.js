f0c5c0cd2c750586f085c0a4589cbec120aa472f
Carla H <hetterichcarla@gmail.com>
2024-05-19 23:31:59 +0200

    estudos js

---
+++ 3 Step/Javascript-Conceitos/assets/js/script.js
@@ -0,0 +1,30 @@
+// comentários
+/* comentários múltiplas linhas */
+
+// Usando Console
+console.log('Usando o Node e o Console!');
+
+  
+//declarando as variáveis
+let userId = 4567;
+let userName = 'usuarioJavascript';
+let userLogged = true;
+let userEmail;
+
+//exibindo no console
+console.log(userId);
+console.log(userName);
+console.log(userLogged);
+console.log(userEmail);
+
+//declarando as constantes
+const DATA_NASCIMENTO = '01/01/1995';
+const CPF = '252.433.368-04';
+const RG_SP = '29.365.924-6';
+
+//exibindo no console
+console.log(DATA_NASCIMENTO);
+console.log(CPF);
+console.log(RG_SP);
+
+// Tipos de Dados
