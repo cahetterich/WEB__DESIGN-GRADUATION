6470f6ebc183fbe8d6242ccd878b86e653aa65e3
Carla H <hetterichcarla@gmail.com>
2024-05-23 22:50:18 +0200

    js - functions

---
+++ 3 Step/Javascript-Intermediario/assets/js/script.js
@@ -0,0 +1,27 @@
+console.log('---------------------------------------');
+// Functions simples
+console.log('--------- Functions simples -----------');
+
+function soma() {
+    let primeiroNumero = 20;
+    let segundoNumero = 30;
+    console.log(primeiroNumero + segundoNumero);
+}
+//argumento
+soma (); // chamamos a função com argumento, para mostrar o console.log
+
+console.log('---------------------------------------');
+// Functions com parâmetros
+console.log('------ Function com parâmetros --------');
+
+function multiplica(valor1, valor2) {
+    let resultado = valor1 * valor2;
+    console.log(`O resultado da função é: ${resultado}`);
+}
+//argumentos
+multiplica(10,2);
+multiplica(9,5);
+
+console.log('---------------------------------------');
+// Functions com parâmetros
+console.log('------ ********* --------');
