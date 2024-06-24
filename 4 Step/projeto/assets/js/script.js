4d7e5f65de50fd548944250e7cf424de8b21466d
Carla H <hetterichcarla@gmail.com>
2024-06-24 22:40:23 +0200

    API Google Maps

--- 4 Step/projeto/assets/js/script.js
+++ 4 Step/projeto/assets/js/script.js
@@ -8,15 +8,67 @@ inputCheck.addEventListener('click', () => {
 })
 
 
-//  Script para inicialização do carrossel 
+
+// Script para inicialização do carrossel
 $(document).ready(function(){
     $('.items').slick({
         infinite: true,
         lazyLoad: 'ondemand',
-        slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 768, // <= 768px
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 992, // <= 992px
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 1200, // <= 1200px
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 1400, // <= 1400px
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: Number.MAX_VALUE, // > 1400px
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4
+                }
+            }
+        ]
     });
 });
 
 
 // API Google
+
+// Script para inicializar o mapa 
+
+function initMap() {
+    var location = {lat: -23.55052, lng: -46.633308}; // Coordenadas de São Paulo, SP (modifique conforme necessário)
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 19,
+        center: location
+    });
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+}
