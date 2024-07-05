691c475d26d2c593e95871623732681bf435beef
Carla H <hetterichcarla@gmail.com>
2024-07-05 22:32:24 +0200

    java teste

--- 4 Step/Java/Teste/src/OperadoresAritmeticos.java
+++ 4 Step/Java/Teste/src/OperadoresAritmeticos.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class OperadoresAritmeticos {
+public class OperadoresAritmeticos {
+
+    public static void main(String[] args) {
+
+        //Operadores Aritméticos (+, -, *, /, %, ++, --)
+        int x = 10;
+        int y = 5;
+        int soma = x + y;
+        System.out.println(soma);
+
+        int multiplicacao = x * 2;
+        System.out.println(multiplicacao);
+        System.out.println(x / 2);
+        System.out.println(y / 2); // resultado é 2, por conta de ser um numero inteiro
+
+        float f = 5;
+        System.out.println(f / 2); // resultado é 2.5, por conta de ser um float
+
+        int resto1 = x % 2;
+        System.out.println(resto1); // = 0
+
+        int resto2 = y % 2;
+        System.out.println(resto2); // = 1
+
+        x++; // incrementa + 1 no x
+        System.out.println(x); // = 11
+
+        y--; // decremento - 1 no y
+        System.out.println(y); // = 4
+
+        int a = 10;
+        int b = 3;
+
+        int soma1 = a + b; // 10+3 = 13
+        int subtracao = a -b; // 10-3 = 7
+        int multiplicacao1 = a * b; // 10 *3 = 30
+        int divisao = a/b; // 10/3 = 3
+        int modulo = a % b; // 10 % 3 = 1 (resto da divisão)
+
+        System.out.println("Soma: "+ soma1);
+        System.out.println("Subtração: "+ subtracao);
+        System.out.println("Multiplicação: "+ multiplicacao1);
+        System.out.println("Divisão: "+ divisao);
+        System.out.println("Módulo: "+ modulo);
+
+    }
 }
---
+++ 4 Step/Java/Teste/src/VariaveisExemplo.java
@@ -0,0 +1,30 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class VariaveisExemplo {
+    public static void main(String[] args) {
+
+        //Tipos primitivos
+        // byte, short, int, long (numeros inteiros)
+        int idade;
+        idade = 22;
+
+        // float, double (precisão maior) (numeros fracionarios)
+        float altura = 1.80f; //(numero tipo f)
+
+        // char (alfanumerico)
+        char genero = 'M';
+
+        // boolean (true o false)
+        boolean ehEstudante = true;
+
+        //Tipo de referência
+        // String (texto)
+        String nome = "Carla Hetterich";
+
+        // Exibindo os valores das variáveis
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Gênero: " + genero);
+        System.out.println("É estudante? " + ehEstudante);
+    }
+}
