f5e445f472ee069a4bcd203fbe972c51c9db8373
Carla H <hetterichcarla@gmail.com>
2024-07-06 14:45:55 +0200

    java

--- 4 Step/Java/Teste/src/Calculadora.java
+++ 4 Step/Java/Teste/src/Calculadora.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Calculadora {
+import java.util.Scanner;
+
+public class Calculadora {
+
+        public static void main(String[] args) {
+
+                Scanner leitor = new Scanner(System.in);
+
+                System.out.println("Digite o nome do aluno: ");
+                String nome = leitor.nextLine();
+
+                //Calcular a média de um aluno
+                System.out.println("Digite a nota 1: ");
+                float nota1 = leitor.nextFloat();
+
+                System.out.println("Digite a nota 2: ");
+                float nota2 = leitor.nextFloat();
+
+                System.out.println("Digite a nota 3: ");
+                float nota3 = leitor.nextFloat();
+
+                float media = (nota1 + nota2 + nota3)/3;
+
+                System.out.println("O aluno " + nome + " possui a média: " + media);
+
+                leitor.close();
+
+        }
 }
