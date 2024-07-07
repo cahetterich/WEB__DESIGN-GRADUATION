2399c4a3b8655b9e8c3d7b410d5fb973fba6850a
Carla H <hetterichcarla@gmail.com>
2024-07-07 22:13:56 +0200

    java estudos

--- 4 Step/Java/Teste/src/DecisaoIf.java
+++ 4 Step/Java/Teste/src/DecisaoIf.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class DecisaoIf {
+
+import java.util.Scanner;
+
+public class DecisaoIf {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean ensolarado, finalDeSemana;
+
+        // Receber informações do usuário sobre as condições
+        System.out.print("Está ensolarado? (true para sim / false para não): ");
+        ensolarado = scanner.nextBoolean();
+
+        System.out.print("É final de semana? (true para sim / false para não): ");
+        finalDeSemana = scanner.nextBoolean();
+
+        // Verificar as condições para decidir o destino do passeio
+        if (ensolarado && finalDeSemana) {
+            System.out.println("Vamos à praia para aproveitar o sol!");
+        }
+        else {
+            System.out.println("Vamos ao parque local!");
+        }
+    }
 }
