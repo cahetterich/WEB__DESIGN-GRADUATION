60a3b46cb06113b80aa60c15ba31a070241e749c
Carla H <hetterichcarla@gmail.com>
2024-07-16 21:58:49 +0200

    const java

--- 4 Step/Java/OrientacaoObjetos/src/PersonagemMagico.java
+++ 4 Step/Java/OrientacaoObjetos/src/PersonagemMagico.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class PersonagemMagico {
+public class PersonagemMagico {
+
+    //classes
+    String nome;
+
+    int nivelEnergia;
+
+    String poderMagico;
+
+    //método implementado
+    public PersonagemMagico(String nome, int nivelEnergia, String poderMagico) {
+        this.nome = nome;
+        this.nivelEnergia = nivelEnergia;
+        this.poderMagico = poderMagico;
+    }
+
+    public PersonagemMagico(String nome){
+        this.nome = nome;
+
+        
+
+    }
+
 }
