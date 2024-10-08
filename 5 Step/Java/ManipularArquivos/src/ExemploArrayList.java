b3e4436ac57749115b51b7702df053bbad5e731e
Carla H <hetterichcarla@gmail.com>
2024-10-08 23:24:08 +0200

    ArrayList

--- 5 Step/Java/ManipularArquivos/src/ExemploArrayList.java
+++ 5 Step/Java/ManipularArquivos/src/ExemploArrayList.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class ExemploArrayList {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExemploArrayList {
+    public static void main(String[] args) {
+        // Declaração e inicialização de um ArrayList de Strings
+        List<String> frutas = new ArrayList<>();
+        // Adicionando elementos à lista
+        frutas.add("Maçã");
+        frutas.add("Banana");
+        frutas.add("Morango");
+        // Exibindo os elementos da lista
+        System.out.println(frutas);
+    }
 }
