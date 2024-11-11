aca4a7233491f235bca19d20ea61de84c8d0d5db
Carla H <hetterichcarla@gmail.com>
2024-11-12 00:51:35 +0100

    ideia

---
+++ 7 Step/global2/assets/js/script.js
@@ -0,0 +1,19 @@
+function openQuiz() {
+    alert("Abrir Quiz sobre Energia");
+}
+
+function openMemoryGame() {
+    alert("Abrir Jogo da Memória");
+}
+
+function openInfo() {
+    alert("Abrir Informações e Links");
+}
+
+function openDragAndDrop() {
+    alert("Abrir Jogo de Arrastar e Soltar");
+}
+
+function openColoringGame() {
+    alert("Abrir Jogo de Colorir");
+}
