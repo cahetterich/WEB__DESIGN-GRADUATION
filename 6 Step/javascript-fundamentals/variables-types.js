a5489714fb75518035dc463f4404c1c3a7a5e59a
Carla H <hetterichcarla@gmail.com>
2025-01-28 20:25:47 +0100

    variables

---
+++ 6 Step/javascript-fundamentals/variables-types.js
@@ -0,0 +1,23 @@
+//TYPES
+console.log(typeof 'fiap'); //string
+console.log(typeof 10); //number
+console.log(typeof true); //boolean
+console.log(typeof {}); //objetc
+console.log(typeof []); //object - list
+console.log(typeof undefined); //undefined - sem valor
+console.log(typeof null); // objetc - valor nulo
+
+//VARIABLES
+let value1 = 1; // variavel altera o valor
+const value2 = 2; // constante não altera valor
+
+console.log(`A soma dos valores 1 e 2 é: ${value1 + value2}.`);
+
+value1 = 5; // alterando o value1
+
+console.log(`O novo valor da 'variável 1' é: ${value1}`); //altera o valor da variavel
+
+let value3 = 'javascript';
+let value4 = 'é legal';
+
+console.log(value3 + value4); //concatenação de strings
