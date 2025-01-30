72aaed6dbfea0d093445f8e54915a2d83152ceea
Carla H <hetterichcarla@gmail.com>
2025-01-30 04:49:49 +0100

    conditionals

---
+++ 6 Step/javascript-fundamentals/conditionals.js
@@ -0,0 +1,11 @@
+const valueOne = 10;
+const valueTwo = 20;
+
+if (valueOne > valueTwo) {
+    console.log(valueOne + " is greater than " + valueTwo);
+    console.log(`O valor ${valueOne} é maior que o valor ${valueTwo}`); 
+     // nesse caso não vai aparecer nada, pois 10 não é maior do que 20
+} else {
+    console.log(`O valor ${valueOne} é menor que o valor ${valueTwo}`);
+}
+
