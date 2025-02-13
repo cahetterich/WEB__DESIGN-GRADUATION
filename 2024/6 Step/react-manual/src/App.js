2fc63a9f6e47516d1a2e331e06c9b59cc58dccaa
Carla H <hetterichcarla@gmail.com>
2025-02-13 23:06:53 +0100

    react config

---
+++ 2024/6 Step/react-manual/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+export function App() {
+	return (
+		<h1>
+            Olá, mundão!
+            <br />
+            Essa é a minha aplicação React construída do zero!
+            </h1>
+	);
+}
+export default App;
