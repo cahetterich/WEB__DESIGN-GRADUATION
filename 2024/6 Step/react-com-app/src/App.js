86886ae95afbc0f3ad627adace16b82ab5355435
Carla H <hetterichcarla@gmail.com>
2025-03-05 21:58:22 +0100

    new

--- 2024/6 Step/react-com-app/src/App.js
+++ 2024/6 Step/react-com-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Consegui instalar os App do React!
+          Aprenda a utilizar o React!
         </p>
         <a
           className="App-link"
