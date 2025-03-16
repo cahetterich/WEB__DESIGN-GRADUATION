20c6b089a49a15ed5c8cd55e82474c15e34fd176
Carla H <hetterichcarla@gmail.com>
2025-03-17 00:58:56 +0100

    estrutura inicial

--- 2025/2 Step/projeto-reuse/src/App.js
+++ 2025/2 Step/projeto-reuse/src/App.js
@@ -1,34 +1,20 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { View, Text } from 'react-native';
-import LoginScreen from './screens/LoginScreen.js';
-
-const Stack = createStackNavigator();
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  console.log("🚀 App.js foi carregado!");
-
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={LoginScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
-// import React from 'react';
-// import { View, Text } from 'react-native';
-
-// export default function App() {
-//   console.log("🚀 O App.js está rodando!");
-
-//   return (
-//     <View>
-//       <Text style={{ fontSize: 24, textAlign: 'center', marginTop: 50 }}>
-//         🚀 O App está funcionando!
-//       </Text>
-//     </View>
-//   );
-// }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
